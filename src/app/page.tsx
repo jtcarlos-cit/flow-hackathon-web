b3ea2e282084e11820d1382de09dd27c54e0a0a7
Calil Christopher <calil.jaudian@flightcentre.com>
2026-01-06 09:43:06 +0800

    style: removed xxx from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <main className="mb-8">
       <div className="max-w-screen-xl mx-auto py-3">
-        <h2 className="text-2xl font-bold tracking-tight">Welcome to iTasksxxxxx</h2>
+        <h2 className="text-2xl font-bold tracking-tight">Welcome to iTasks</h2>
         <p className="text-muted-foreground">
           Simplify your task management with ease and efficiency.
         </p>
