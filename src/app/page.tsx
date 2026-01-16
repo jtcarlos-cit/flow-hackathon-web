06446223e7f749d17fbd2222b7266bb4fa57cc01
ding-cit <ding.catubag@flightcentre.com>
2026-01-16 15:43:02 +0800

    PM-63: Slowness fix (#12)
    Co-authored-by: ding-cit <dingcatubag@ciandt.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-import Box from '@/components/box/box';
+import Box from "@/components/box/box";
 
 const Home = () => {
   return (
-    <main className='mb-8'>
-      <div className='max-w-screen-xl mx-auto py-3'>
-        <h2 className='text-2xl font-bold tracking-tight'>Welcome to iTasks</h2>
-        <p className='text-muted-foreground'>
+    <main className="mb-8">
+      <div className="max-w-screen-xl mx-auto py-3">
+        <h2 className="text-2xl font-bold tracking-tight">Welcome to iTasks</h2>
+        <p className="text-muted-foreground">
           Simplify your task management with ease and efficiency. This website
-          is reviewed by AI for faster implementation.
+          is reviewed by AI for faster implementation. slowness
         </p>
       </div>
-      <div className='max-w-screen-xl mx-auto'>
+      <div className="max-w-screen-xl mx-auto">
         <Box />
       </div>
     </main>
