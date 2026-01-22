ade2c91a832ec2a372eac67759614e54521107a2
ding-cit <ding.catubag@flightcentre.com>
2026-01-22 15:57:21 +0800

    PM-68: Payment fix (#19)
    * PM-68: Payment fix
    
    * PM-68: Fail
    
    * PM-68: removing console
    
    ---------
    
    Co-authored-by: ding-cit <dingcatubag@ciandt.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const Home = () => {
         <h2 className="text-2xl font-bold tracking-tight">Welcome to iTasks</h2>
         <p className="text-muted-foreground">
           Simplify your task management with ease and efficiency. This website
-          is reviewed by AI for faster implementation. slowness fix
+          is reviewed by AI for faster implementation.
         </p>
       </div>
       <div className="max-w-screen-xl mx-auto">
